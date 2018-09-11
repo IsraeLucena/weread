21ebcaa20dfe7df28a6f68345118f8b1d3cd8c66
israel.lucena <israel.lucena@mv.com.br>
2018-09-11 18:13:11 -0300

    resolve conflicts

---
+++ app/src/main/java/com/example/israel/rssread/Item.kt
@@ -0,0 +1,32 @@
+package com.example.israel.rssread
+
+import android.os.Parcel
+import android.os.Parcelable
+
+class Item(val id: String, val name: String, val desc: String): Parcelable {
+
+    constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString()) {}
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(id)
+        parcel.writeString(name)
+        parcel.writeString(desc)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Item> {
+        override fun createFromParcel(parcel: Parcel): Item {
+            return Item(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Item?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
